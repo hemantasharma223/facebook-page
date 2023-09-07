8c20b99830a122d1a6e7df52d98926cc5a87a506
hemantasharma223 <hemantasharma223@gmail.com>
2023-09-07 16:51:43 +0545

    added registration page

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const openButton = document.getElementById('open-registration-form');
 const registrationForm = document.getElementById('registration-form');
 
+
 openButton.addEventListener('click', () => {
     registrationForm.style.display = 'block';
 });
+
+
+const closeButton = document.getElementById('close-btn');
+closeButton.addEventListener('click', () => {
+    registrationForm.style.display = 'none';
+}); 
